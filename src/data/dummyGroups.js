1c2820de7446a894d202c53f60c25a0af589db49
RITANKAR SAHA <ritankar.saha786@gmail.com>
2025-03-30 09:44:38 +0530

    (temp-fix): Removed un-necessary queries for now. Will add them later

--- src/data/dummyGroups.js
+++ src/data/dummyGroups.js
@@ -10,11 +10,11 @@ export const dummyGroups = [
         name: 'Select All from Customers',
         sql: 'SELECT * FROM Customers;',
       },
-      {
-        id: 'cust_germany',
-        name: 'Select Customers from Germany',
-        sql: "SELECT * FROM Customers WHERE country='Germany';",
-      },
+      // {
+      //   id: 'cust_germany',
+      //   name: 'Select Customers from Germany',
+      //   sql: "SELECT * FROM Customers WHERE country='Germany';",
+      // },
     ],
   },
   {
@@ -26,11 +26,11 @@ export const dummyGroups = [
         name: 'Select All from Products',
         sql: 'SELECT * FROM Products;',
       },
-      {
-        id: 'prod_filtered',
-        name: 'Select Products with UnitPrice > 18 and UnitStock > 15',
-        sql: 'SELECT * FROM Products WHERE unitPrice > 18 AND unitsInStock > 15;',
-      },
+      // {
+      //   id: 'prod_filtered',
+      //   name: 'Select Products with UnitPrice > 18 and UnitStock > 15',
+      //   sql: 'SELECT * FROM Products WHERE unitPrice > 18 AND unitsInStock > 15;',
+      // },
     ],
   },
   {
@@ -42,11 +42,11 @@ export const dummyGroups = [
         name: 'Select All from Orders',
         sql: 'SELECT * FROM Orders;',
       },
-      {
-        id: 'ord_ships',
-        name: 'Select Orders with Freight > 10 and ShipCountry in (France, Germany, Brazil, Belgium)',
-        sql: "SELECT * FROM Orders WHERE freight > 10 AND shipCountry IN ('France', 'Germany', 'Brazil', 'Belgium');",
-      },
+      // {
+      //   id: 'ord_ships',
+      //   name: 'Select Orders with Freight > 10 and ShipCountry in (France, Germany, Brazil, Belgium)',
+      //   sql: "SELECT * FROM Orders WHERE freight > 10 AND shipCountry IN ('France', 'Germany', 'Brazil', 'Belgium');",
+      // },
     ],
   },
 ]
